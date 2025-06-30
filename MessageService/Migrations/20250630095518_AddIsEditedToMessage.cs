b6d3912027f2e185251b504115dc896ce6035544
Vasilev Ruslan <ruslan.vasilev@icl-services.com>
2025-06-30 13:43:57 +0300

    migration

---
+++ MessageService/Migrations/20250630095518_AddIsEditedToMessage.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessageService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsEditedToMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEdited",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "IsEdited",
+                table: "Messages");
+        }
+    }
+}
